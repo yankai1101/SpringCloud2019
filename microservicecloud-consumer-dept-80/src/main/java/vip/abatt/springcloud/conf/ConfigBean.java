b4a7868e02bcba0a2039e4283421e808a5c8fa3a
yankai1101 <yankai11101@163.com>
2019-09-05 15:05:06 +0800

    闫凯-第3次提交： 1.编写部门服务消费者microservicecloud-consumer-dept-80

---
+++ microservicecloud-consumer-dept-80/src/main/java/vip/abatt/springcloud/conf/ConfigBean.java
@@ -0,0 +1,13 @@
+package vip.abatt.springcloud.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConfigBean {
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ microservicecloud-consumer-dept-80/src/main/java/vip/abatt/springcloud/controller/DeptConsumerController.java
@@ -0,0 +1,35 @@
+package vip.abatt.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import vip.abatt.springcloud.entities.Dept;
+
+import java.util.List;
+
+@RestController
+public class DeptConsumerController {
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping(value = "/consumer/dept/add")
+    public boolean add(Dept dept) {
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+    }
+
+    @GetMapping(value = "/consumer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
+    }
+
+    @GetMapping(value = "/consumer/dept/list")
+    public List<Dept> list() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+
+}
