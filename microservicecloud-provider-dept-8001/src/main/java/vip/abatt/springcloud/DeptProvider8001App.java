f41373f97b9bda6e28bc5ac00c1c8a3958b05aa6
yankai1101 <yankai11101@163.com>
2019-09-05 10:37:04 +0800

    闫凯-第2次提交： 1.编写部门服务提供者microservicecloud-provider-dept-8001

---
+++ microservicecloud-provider-dept-8001/src/main/java/vip/abatt/springcloud/DeptProvider8001App.java
@@ -0,0 +1,11 @@
+package vip.abatt.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptProvider8001App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8001App.class, args);
+    }
+}
