550543405fa9566ca29129e972b975b1610177e2
yankai1101 <yankai11101@163.com>
2019-09-06 16:41:48 +0800

    闫凯-第6次提交： 1.Ribbon初步配置

--- microservicecloud-consumer-dept-80/src/main/java/vip/abatt/springcloud/DeptConsumer80App.java
+++ microservicecloud-consumer-dept-80/src/main/java/vip/abatt/springcloud/DeptConsumer80App.java
@@ -2,8 +2,10 @@ package vip.abatt.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptConsumer80App {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer80App.class, args);
