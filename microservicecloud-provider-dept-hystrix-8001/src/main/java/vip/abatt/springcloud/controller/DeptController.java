cf6a59cd3896437a474964f7d0b8374da389bc70
yankai1101 <yankai11101@163.com>
2019-09-07 19:15:14 +0800

    闫凯-第10次提交： 1.测试服务熔断

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/vip/abatt/springcloud/controller/DeptController.java
@@ -0,0 +1,33 @@
+package vip.abatt.springcloud.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+import vip.abatt.springcloud.entities.Dept;
+import vip.abatt.springcloud.service.DeptService;
+
+import java.util.List;
+
+@RestController
+public class DeptController {
+    @Autowired
+    private DeptService service;
+
+    @GetMapping(value = "/dept/get/{id}")
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
+    public Dept get(@PathVariable("id") Long id) {
+        Dept dept = this.service.get(id);
+        if (null == dept) {
+            throw new RuntimeException("该ID：" + id + "没有没有对应的信息");
+        }
+        return dept;
+    }
+
+    public Dept processHystrix_Get(@PathVariable("id") Long id) {
+        return new Dept().setDeptno(id)
+                .setDname("该ID：" + id + "没有没有对应的信息,null--@HystrixCommand")
+                .setDb_source("no this database in MySQL");
+    }
+}
